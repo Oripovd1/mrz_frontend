618f2cf77275e5d17d321281d3cd8d636c5872b4
Oripovd1 <oripovd1@gmail.com>
2021-05-09 22:44:36 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,145 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { Button, message, Typography, Upload } from 'antd'
+import { LoadingOutlined, PlusOutlined } from '@ant-design/icons'
+import './App.scss'
+const { Title } = Typography
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [loading, setLoading] = useState()
+  const [imageUrl, setImageUrl] = useState()
+  const [data, setData] = useState(null)
+
+  function getBase64(img, callback) {
+    const reader = new FileReader()
+    reader.addEventListener('load', () => callback(reader.result))
+    reader.readAsDataURL(img)
+  }
+
+  function beforeUpload(file) {
+    const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
+    if (!isJpgOrPng) {
+      message.error('You can only upload JPG/PNG file!')
+    }
+    const isLt2M = file.size / 1024 / 1024 < 2
+    if (!isLt2M) {
+      message.error('Image must smaller than 2MB!')
+    }
+    return isJpgOrPng && isLt2M
+  }
+
+  const handleChange = (info) => {
+    if (info.file.status === 'uploading') {
+      setLoading(true)
+      return
+    }
+    console.log('info => ', info)
+
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, (imageUrl) => {
+        setImageUrl(imageUrl)
+        setLoading(false)
+      })
+    }
+
+    let formdata = new FormData()
+    formdata.append('image', info.file.originFileObj, '2021-05-07 14.36.46.jpg')
+
+    let requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow',
+    }
+
+    fetch('https://mrz-service.herokuapp.com/upload', requestOptions)
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.fields)
+        console.log(result)
+      })
+      .catch((error) => console.log('error', error))
+  }
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Загрузить паспорт</div>
     </div>
-  );
+  )
+  console.log('data => ', data)
+
+  return (
+    <div className='App'>
+      <Title level={3}>Данные паспорта</Title>
+      <div className='upload'>
+        <Upload
+          name='avatar'
+          listType='picture-card'
+          className='avatar-uploader'
+          showUploadList={false}
+          // beforeUpload={beforeUpload}
+          onChange={handleChange}
+          action='https://www.mocky.io/v2/5cc8019d300000980a055e76'
+        >
+          {imageUrl ? (
+            <img src={imageUrl} alt='avatar' style={{ width: '100%' }} />
+          ) : (
+            uploadButton
+          )}
+        </Upload>
+      </div>
+      <div className='card'>
+        <Title level={4}>Личные данные</Title>
+        <ul>
+          <li>
+            <b>Имя</b>
+            <span>{data?.firstName}</span>
+          </li>
+          <li>
+            <b>Фамилия</b>
+            <span>{data?.lastName}</span>
+          </li>
+          <li>
+            <b>Отчество</b>
+            <span>{data?.nationality}</span>
+          </li>
+          <li>
+            <b>Место рождения</b>
+            <span>{data?.issuingState}</span>
+          </li>
+          <li>
+            <b>Дата рождения</b>
+            <span>{data?.birthDate}</span>
+          </li>
+          <li>
+            <b>Пол</b>
+            <span>{data?.sex}</span>
+          </li>
+        </ul>
+      </div>
+
+      <div className='card'>
+        <Title level={4}>Паспортные данные</Title>
+        <ul>
+          <li>
+            <b>Серия паспорта</b>
+            <span>{data?.documentNumber.slice(0, 2)}</span>
+          </li>
+          <li>
+            <b>Номер паспорта</b>
+            <span>{data?.documentNumber}</span>
+          </li>
+          <li>
+            <b>Срок действия</b>
+            <span>{data?.expirationDate}</span>
+          </li>
+        </ul>
+      </div>
+
+      <Button size='large' type='primary' className='button'>
+        Продолжить
+      </Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
